1a0c6e75cf4a2004e8426797316de9f2c00a9d32
Erick Orlando <erickorlando@outlook.com>
2017-05-03 10:44:09 -0500

    Paquetes nuget y comentarios iniciales

--- ReporteComoCorreo/Program.cs
+++ ReporteComoCorreo/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/* 
+ * Este proyecto pretende mostrar como ejecutar un reporte RDLC (Reporting Services)
+ * como un archivo adjunto PDF en un correo electrónico en tiempo de ejecución
+ * sin necesidad de hacer un preview del reporte.
+ * Autor: Erick Orlando (http://t.me/erickorlando)
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +17,7 @@ namespace ReporteComoCorreo
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
